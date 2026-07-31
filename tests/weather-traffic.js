c0db1d1507f7f9d44bd6fa55446d53df813ba848
Wael-Bns <w.waelbenslimene004@gmail.com>
2026-07-31 13:47:17 +0100

    replace image tag with the github sha

--- tests/weather-traffic.js
+++ tests/weather-traffic.js
@@ -7,7 +7,7 @@ export const options = {
     weather_traffic: {
       executor: 'constant-vus',
       vus: 100,
-      duration: '2m',
+      duration: '6m',
     },
   },
   thresholds: defaultThresholds,
